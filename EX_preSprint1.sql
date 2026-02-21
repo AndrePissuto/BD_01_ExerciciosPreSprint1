dcc8e9d37cfc64ae2dfdbb5d96d3b6165155e525
NogPiss <gorduranogueirapissuto@gmail.com>
2026-02-20 21:41:53 -0300

    add realizacao dos ex 1 e 2

--- EX_preSprint1.sql
+++ EX_preSprint1.sql
@@ -1,4 +1,6 @@
+/*Ex 1*/
 create database game_store;
+use game_store;
 
 create table jogo(
 	id int primary key auto_increment,
@@ -10,7 +12,6 @@ create table jogo(
     quantidade int
 );
 
-alter table jogo add constraint chkNota check(nota >= 0 and nota <= 10);
 alter table jogo add constraint chkQuantidade check(quantidade >= 0);
 
 insert into jogo values 
@@ -37,4 +38,68 @@ select * from jogo where lancamento > '2015-01-01';
 
 select * from jogo where nome like '%a%' and midia = 'Fisica';
 
+select * from jogo where diretor not like '%e%';
 
+alter table jogo add constraint chkNota check(nota >= 0 and nota <= 10);
+
+select * from jogo where genero = 'tiro' and quantidade > 0;
+
+delete from jogo where quantidade = 0;
+
+alter table jogo rename column diretor to criador;
+
+/*Ex 2*/
+
+create database olimpiedas;
+use olimpiedas;
+
+create table esporte(
+	id int primary key auto_increment,
+    nome varchar(40),
+    categoria varchar(20),
+    numJogadores int,
+    estreia date,
+    paisOrigem varchar(30)
+);
+
+alter table esporte add constraint chkCategoria check(categoria = 'Individual' or categoria = 'Coletivo');
+
+insert into esporte values 
+(default, 'Futebol', 'Coletivo', 11, '1863-10-26', 'Inglaterra'),
+(default, 'Basquete', 'Coletivo', 5, '1891-12-21', 'Estados Unidos'),
+(default, 'Tênis', 'Individual', 1, '1873-01-01', 'Inglaterra'),
+(default, 'Vôlei', 'Coletivo', 6, '1895-02-09', 'Estados Unidos'),
+(default, 'Handebol', 'Coletivo', 7, '1917-01-01', 'Alemanha');
+
+alter table esporte add column popularidade decimal(3,1);
+
+alter table esporte  add constraint chkPopularidade check(popularidade >= 0 and popularidade <= 10);
+
+desc esporte;
+
+update esporte set popularidade = 10 where id = 1;
+update esporte set popularidade = 10 where id = 2;
+update esporte set popularidade = 6.5 where id = 3;
+update esporte set popularidade = 7 where id = 4;
+update esporte set popularidade = 5.5 where id = 5;
+
+select * from esporte order by popularidade;
+
+select * from esporte where estreia >= '2000-01-01';
+
+update esporte set estreia = '1897-04-06' where id = 1;
+update esporte set estreia = '1897-04-06' where id = 3;
+update esporte set estreia = '1897-04-06' where id = 4;
+update esporte set estreia = '1897-04-06' where id = 2;
+
+alter table esporte add constraint chkEstreia check(estreia >= '1896-04-06' and estreia <= '2026-02-20');
+
+alter table esporte drop constraint  chkCategoria;
+
+select * from esporte where nome like '_a%';
+
+select * from esporte where numJogadores >= 4 and numJogadores <= 11;
+
+delete from esporte where id in (1,3,5);
+
+select * from esporte
